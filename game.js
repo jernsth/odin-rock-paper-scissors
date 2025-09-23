990819fd7cac29a61b6cb627c3b5f183e9158d55
j.hermsen <j.hermsen@m3connect.de>
2025-09-23 13:27:18 +0200

    Finished rock-paper-scissors game

---
+++ game.js
@@ -0,0 +1,62 @@
+let humanScore=0;
+let computerScore=0;
+
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3) + 1;
+
+    switch(x) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+
+    return "Invalid";
+}
+
+function getHumanChoice() {
+    let x = prompt("Choose between Rock, Paper and Scissors!", "Your choice");
+    return x;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (String(humanChoice).toLowerCase() === String(computerChoice).toLowerCase()) {
+        alert("Computer's choice: " + computerChoice);
+        alert("No winner!");
+    } else if (String(humanChoice).toLowerCase() === "rock" && String(computerChoice).toLowerCase() === "scissors") {
+        alert("Computer's choice: " + computerChoice);
+        alert("You win! Rock beats Scissors");
+        humanScore++;
+    } else if (String(humanChoice).toLowerCase() === "rock" && String(computerChoice).toLowerCase() === "paper") {
+        alert("Computer's choice: " + computerChoice);
+        alert("You lose! Paper beats Rock");
+        computerScore++;
+    } else if (String(humanChoice).toLowerCase() === "paper" && String(computerChoice).toLowerCase() === "rock") {
+        alert("Computer's choice: " + computerChoice);
+        alert("You win! Paper beats Rock");
+        humanScore++;
+    } else if (String(humanChoice).toLowerCase() === "paper" && String(computerChoice).toLowerCase() === "scissors") {
+        alert("Computer's choice: " + computerChoice);
+        alert("You lose! Scissors beats Paper");
+        computerScore++;
+    } else if (String(humanChoice).toLowerCase() === "scissors" && String(computerChoice).toLowerCase() === "paper") {
+        alert("Computer's choice: " + computerChoice);
+        alert("You win! Scissors beats Paper");
+        humanScore++;
+    } else if (String(humanChoice).toLowerCase() === "scissors" && String(computerChoice).toLowerCase() === "rock") {
+        alert("Computer's choice: " + computerChoice);
+        alert("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+}
+
+function playGame() {
+    for (let i = 0; i<5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+    alert("Final Score: Player " + humanScore + " Computer: " + computerScore);
+}
+
+playGame();
